cd5dd39377438141759c1eddc245f7fda119d668
Marlon Clay <luciousmc@icloud.com>
2022-10-27 20:23:54 -0700

    feat(): add header links

--- pages/components/header.tsx
+++ pages/components/header.tsx
@@ -1,4 +1,5 @@
 import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react'
+import Link from 'next/link'
 
 type Props = {}
 
@@ -9,8 +10,8 @@ function Header({}: Props) {
 
   return (
     <header>
-      <nav>
-        <div>
+      <nav className="flex justify-between">
+        <div className="flex items-center space-x-2 text-sm">
           {address ? (
             <button className="connectWalletBtn" onClick={disconnect}>
               Hi, {address.slice(0, 5) + '...' + address.slice(-4)}
@@ -20,7 +21,25 @@ function Header({}: Props) {
               Connect your wallet
             </button>
           )}
+
+          <p className="headerLinks">Daily Deals</p>
+          <p className="headerLinks">Help & Contact</p>
         </div>
+
+        <ul className="flex items-center space-x-4 text-sm">
+          <li className="headerLinks">
+            <Link href="#">Ship to</Link>
+          </li>
+          <li className="headerLinks">
+            <Link href="#">Sell</Link>
+          </li>
+          <li className="headerLinks">
+            <Link href="#">Watchlist</Link>
+          </li>
+          <li>
+            <Link href="">Add to Inventory</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   )
